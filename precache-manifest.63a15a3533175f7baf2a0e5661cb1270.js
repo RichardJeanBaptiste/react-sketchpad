423b84d924cefffbe1e9e5dff012a8b83752f977
Richard Jean-Baptiste <Richinbk1@gmail.com>
2019-04-15 21:44:58 -0400

    Updates

---
+++ precache-manifest.63a15a3533175f7baf2a0e5661cb1270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2036f825dda286ba9af3",
+    "url": "/react-sketchpad/static/js/runtime~main.96ee1dbc.js"
+  },
+  {
+    "revision": "0a29bd604cb8377f2d70",
+    "url": "/react-sketchpad/static/js/main.c94a7d93.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/react-sketchpad/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "0a29bd604cb8377f2d70",
+    "url": "/react-sketchpad/static/css/main.ca3ad5d0.chunk.css"
+  },
+  {
+    "revision": "4f21adf9f8510512e565709c1a4a1890",
+    "url": "/react-sketchpad/index.html"
+  }
+];
